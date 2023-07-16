704c72c6256595cc905ef26a19f8e4414555fdbf
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2023-07-16 17:25:07 +0600

    Delete method added to LinkedList Class

--- src/linked_list.py
+++ src/linked_list.py
@@ -34,6 +34,30 @@ class LinkedList:
             self.tail.next_node = new_node  # У текущего хвоста указываем ссылку на новый узел-хвост
             self.tail = new_node            # Новым хвостом становится новый узел
 
+    def delete_beginning(self) -> None:
+        """Удаляет узел с данными из начала связанного списка"""
+        if self.head is None:                   # Если список пуст, то нечего удалять
+            return
+        elif self.head != self.tail:            # Если список состоит более, чем из одного узла,
+            self.head = self.head.next_node     # то головным узлом становится следующий за ним
+        else:
+            self.head = None                    # Если в списке только один узел
+            self.tail = None                    # то удаляем его, очищая весь список
+
+    def delete_at_end(self) -> None:
+        """Удаляет узел с данными из конца связанного списка"""
+        if self.head is None:                   # Если список пуст, то нечего удалять
+            return
+        elif self.head != self.tail:            # Если список состоит более, чем из одного узла,
+            temp = self.head
+            while temp.next_node != self.tail:  # Перебираем все узлы в поисках предпоследнего узла
+                temp = temp.next_node
+            self.tail = temp                    # Предпоследний узел становится конечным в списке
+            self.tail.next_node = None          # и он не ссылается на какой-либо другой узел
+        else:
+            self.head = None                    # Если в списке только один узел
+            self.tail = None                    # то удаляем его, очищая весь список
+
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
         node = self.head
